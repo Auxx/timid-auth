35ebf9817f91a0976b417b3593544dbc4ab475c4
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2016-05-14 12:19:16 +0100

    Added documentation

--- src/tokens/default-redirect.ts
+++ src/tokens/default-redirect.ts
@@ -1,3 +1,17 @@
 import {OpaqueToken} from "@angular/core";
 
+/**
+ * Configuration token used to identify default redirect link for unauthorized routes.
+ *
+ * ### Example (boots.ts)
+ *
+ * ```typescript
+ * bootstrap(AppComponent, [
+ *     ...
+ *     provide(DefaultRedirect, {useValue: ["LoginPage"]})
+ * ]);
+ * ```
+ *
+ * @type {OpaqueToken}
+ */
 export const DefaultRedirect: OpaqueToken = new OpaqueToken("DefaultRedirect");
